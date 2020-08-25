e706530745a2eabca3a5faf04750d28ac74ac715
mckib2 <nicholas.bgp@gmail.com>
2020-08-25 00:20:03 -0700

    Working on linking the shared library extensions. More clang components added.  May need to be building and linking llvm components as well

--- setup.py
+++ setup.py
@@ -3,8 +3,44 @@
 import pathlib
 import platform
 import re
+from collections import namedtuple
 from setuptools import find_packages
 from distutils.core import Extension, setup
+from distutils.command.build_ext import build_ext as _build_ext
+
+from numpy.distutils.misc_util import Configuration
+
+
+class build_ext(_build_ext):
+    def run(self):
+        super().run()
+
+        # All the shared library extensions are now built;
+        # We need to link all the shared objects together
+        for ext in self.extensions:
+            if ext.clang_link:
+
+                # We need 'lib' versions of the shared libraries
+                # that need to be linked
+                build_lib = pathlib.Path(self.build_lib) / 'cythonator'
+                so_to_link = [self.get_ext_filename('clang' + l) for l in ext.clang_link]
+                to_link = []
+                for ii, l in enumerate(ext.clang_link):
+                    to_move = pathlib.Path(
+                        self.compiler.library_filename('clang' + l)).with_suffix(
+                            pathlib.Path(so_to_link[ii]).suffix)
+                    to_link0 = 'clang' + l
+                    self.copy_file(build_lib / so_to_link[ii], build_lib / to_move)
+                    to_link.append(str(to_link0))
+
+                # Link 'lib' versions to the object files of each extension
+                self.compiler.link_shared_lib(
+                    self.compiler.object_filenames([
+                        self.build_temp + '/' + s for s in ext.sources]),
+                    ext.name,
+                    libraries=to_link,
+                    library_dirs=[str(build_lib)],
+                )
 
 
 def _llvm_config(llvm_config_dir, llvm_triple='x86_64-unknown-linux-gnu'):
@@ -73,10 +109,8 @@ def _cmakelists_scrape(base_dir, START_TOKEN, END_TOKEN):
     return [str(base_dir / s.strip()) for s in contents[start_idx:end_idx].split() if s.strip() != '']
 
 
-CLANG_DIR = '/home/nicholas/Documents/llvm-project/clang/'
-LLVM_DIR = '/home/nicholas/Documents/llvm-project/llvm/'
-
-_llvm_config_path = pathlib.Path(LLVM_DIR) / 'include/llvm/Config'
+LLVM_DIR = pathlib.Path('/home/nicholas/Documents/llvm-project/llvm/')
+_llvm_config_path = LLVM_DIR / 'include/llvm/Config'
 _config_dir = pathlib.Path('src/llvm/Config/')
 _config_dir.mkdir(exist_ok=True, parents=True)
 with open(_config_dir / 'llvm-config.h', 'w') as fp:
@@ -84,50 +118,133 @@ with open(_config_dir / 'llvm-config.h', 'w') as fp:
 with open(_config_dir / 'abi-breaking.h', 'w') as fp:
     fp.write(_abi_breaking_config(_llvm_config_path))
 
-_basic_path = pathlib.Path(CLANG_DIR) / 'lib/Basic'
-clangBasic_sources = _cmakelists_scrape(
-    _basic_path,
-    START_TOKEN='add_clang_library(clangBasic',
-    END_TOKEN='${version_inc}',
-)
-clangBasic_include = [
-    'src/',
-    pathlib.Path(CLANG_DIR) / 'include',
-    pathlib.Path(CLANG_DIR) / 'lib/Basic',
-    pathlib.Path(LLVM_DIR) / 'include',
-]
 
-_lex_path = pathlib.Path(CLANG_DIR) / 'lib/Lex'
-clangLex_sources = _cmakelists_scrape(
-    _lex_path,
-    START_TOKEN='add_clang_library(clangLex',
-    END_TOKEN='LINK_LIBS',
-)
-clangLex_include = [
-    'src/',
-    pathlib.Path(CLANG_DIR) / 'include',
-    pathlib.Path(LLVM_DIR) / 'include',
-]
+def configuration(parent_package='', top_path=None):
 
-_ast_path = pathlib.Path(CLANG_DIR) / 'lib/AST'
-clangAST_sources = _cmakelists_scrape(
-    _ast_path,
-    START_TOKEN='add_clang_library(clangAST',
-    END_TOKEN='LINK_LIBS',
-)
-clangAST_include = [
-    'src/',
-    'src/AST/',
-    pathlib.Path(CLANG_DIR) / 'include',
-    pathlib.Path(LLVM_DIR) / 'include',
-]
+    config = Configuration('cythonator', parent_package, top_path)
 
-# ASTMatchers next...
-# Got to get clangParser it looks like...
+    # TODO: we may need to be compiling some LLVM and linking as well...
+
+    lib_t = namedtuple(
+        'lib_t',
+        'name START_TOKEN END_TOKEN extra_library_dirs link_libs actual_path')
+    clang_sources = dict()
+    clang_includes = dict()
+    CLANG_DIR = pathlib.Path('/home/nicholas/Documents/llvm-project/clang/')
+    libs = [
+        lib_t('Basic', None, '${version_inc}', [CLANG_DIR / 'lib/Basic'],
+              [], None),
+        lib_t('Lex', None, None, [], ['Basic'], None),
+        lib_t('AST', None, None, ['src/AST/'],
+              ['Basic',
+               'Lex'], None),
+        lib_t('ASTMatchers', None, None, [],
+              ['Basic',
+               'Lex',
+               'AST'], None),
+        lib_t('Analysis', None, None, [],
+              ['Basic',
+               'Lex',
+               'AST',
+               'ASTMatchers'], None),
+        lib_t('Edit', None, None, [],
+              ['Basic',
+               'Lex',
+               'AST'], None),
+        lib_t('Sema', None, 'DEPENDS', ['src/Sema/'],
+              ['Basic',
+               'Lex',
+               'AST',
+               'Analysis',
+               'Edit'], None),
+        lib_t('Parse', None, None, [],
+              ['Basic',
+               'Lex',
+               'AST',
+               'Sema'], None),
+        lib_t('Serialization', None, 'ADDITIONAL_HEADERS',
+              [CLANG_DIR / 'lib/Serialization'],
+              ['Basic',
+               'Lex',
+               'AST',
+               'Sema'], None),
+        lib_t('Driver', None, 'DEPENDS', [CLANG_DIR / 'lib/Driver'],
+              ['Basic'], None),
+        lib_t('Frontend', None, 'DEPENDS', [],
+              ['Basic',
+               'Lex',
+               'AST',
+               'Sema',
+               'Edit',
+               'Parse',
+               'Driver',
+               'Serialization'], None),
+        lib_t('Rewrite', None, None, [], ['Basic', 'Lex'], None),
+        lib_t('ToolingCore', None, None, [],
+              ['AST',
+               'Basic',
+               'Lex',
+               'Rewrite'], 'Tooling/Core'),
+        lib_t('ToolingInclusions', None, None, [],
+              ['Basic',
+               'Lex',
+               'Rewrite',
+               'ToolingCore'], 'Tooling/Inclusions'),
+        lib_t('Format', None, None, [],
+              ['Basic',
+               'Lex',
+               'ToolingCore',
+               'ToolingInclusions'], None),
+        lib_t('Tooling', None, 'DEPENDS', [],
+              ['AST',
+               'ASTMatchers',
+               'Basic',
+               'Driver',
+               'Format',
+               'Frontend',
+               'Lex',
+               'Rewrite',
+               'Serialization',
+               'ToolingCore'], None),
+    ]
+
+    for l in libs:
+        if l.START_TOKEN is None:
+            START_TOKEN = 'add_clang_library(clang' + l.name
+        else:
+            START_TOKEN = l.START_TOKEN
+        if l.END_TOKEN is None:
+            END_TOKEN = 'LINK_LIBS'
+        else:
+            END_TOKEN = l.END_TOKEN
+
+        _path = l.actual_path if l.actual_path is not None else l.name
+        clang_sources[l.name] = _cmakelists_scrape(
+            pathlib.Path(CLANG_DIR) / 'lib' / _path,
+            START_TOKEN=START_TOKEN,
+            END_TOKEN=END_TOKEN,
+        )
+        clang_includes[l.name] = [
+            'src/',
+            CLANG_DIR / 'include',
+            LLVM_DIR / 'include',
+        ] + l.extra_library_dirs
+
+        ext = config.add_extension(
+            'clang' + l.name,
+            sources=clang_sources[l.name],
+            include_dirs=clang_includes[l.name],
+            language='c++',
+            extra_compile_args=['-Wno-strict-aliasing',
+                                '-Wno-maybe-uninitialized',
+                                '-Wno-comment',
+                                ])
+        setattr(ext, 'clang_link', l.link_libs)
+
+    return config
 
 
 setup(
-    name='cythonator',
     version='0.0.1',
     author='Nicholas McKibben',
     author_email='nicholas.bgp@gmail.com',
@@ -139,36 +256,6 @@ setup(
     keywords='clang cython',
     install_requires=open('requirements.txt', encoding='utf-8').read().split(),
     python_requires='>=3.5',
-
-    ext_modules=[
-        # Extension(
-        #     'clangBasic',
-        #     sources=clangBasic_sources,
-        #     include_dirs=clangBasic_include,
-        #     language='c++',
-        # ),
-        # Extension(
-        #     'clangLex',
-        #     sources=clangLex_sources,
-        #     libraries=['clangBasic'],
-        #     include_dirs=clangLex_include,
-        #     language='c++',
-        # ),
-        # Extension(
-        #     'clangAST',
-        #     sources=clangAST_sources,
-        #     libraries=['clangBasic', 'clangLex'],
-        #     include_dirs=clangAST_include,
-        #     language='c++',
-        # ),
-
-        # Build all sources together to avoid linking problems
-        Extension(
-            'clangAST',
-            sources=clangBasic_sources + clangLex_sources + clangAST_sources,
-            include_dirs=list(set(
-                clangBasic_include + clangLex_include + clangAST_include)),
-            language='c++',
-        )
-    ],
+    **configuration(top_path='').todict(),
+    cmdclass={'build_ext': build_ext},
 )
